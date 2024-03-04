9531bfd8fa61bcfee075016de1a22e1cc132ed70
driptard00 <akintadeseun816@gmail.com>
2024-03-04 19:54:53 +0100

    Initial Commit

---
+++ TadeCard/TadeCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  TadeCardApp.swift
+//  TadeCard
+//
+//  Created by tade on 04/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct TadeCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
